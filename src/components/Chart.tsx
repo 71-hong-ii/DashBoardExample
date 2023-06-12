27d810ebc70c352aade15e5931888489e503df27
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-12 17:36:24 +0900

    modified Chart.tsx

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const Chart = () => {
   return (
     <div>
-      <div className="card-container grid">
-        <h3 className="card-title">team members</h3>
+      <div className="chart">
       </div>
       <span></span>
       
