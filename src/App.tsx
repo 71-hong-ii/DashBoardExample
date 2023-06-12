6fb6daceaa092023c8f1904a60479c39e0a6e964
unknown <cksdyd8350@gmail.com>
2023-06-12 16:53:06 +0900

    added directories

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import { Button } from '@progress/kendo-react-buttons';
 import kendoka from './kendoka.svg';
 import './App.scss';
+import { IntlProvider, load, LocalizationProvider, loadMessages } from '@progress/kendo-react-intl';
 
 function App() {
-  const handleClick = React.useCallback(() => {
-    window.open('https://www.telerik.com/kendo-react-ui/components/', '_blank');
-  }, []);
+  const [contextState, setContextState] = React.useState({
+    localeId: 'en-US',
+    firstName: 'Peter',
+    lastName: 'Douglas',
+    middleName: '',
+    email: 'peter.douglas@progress.com',
+    phoneNumber: '(+1) 8373-837-93-02',
+    avatar: null,
+    isInPublicDirectory: true,
+    biography: '',
+    teamId: 1
+});
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={kendoka} className="App-logo" alt="kendoka" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button
-          themeColor={'primary'}
-          size={"large"}
-          onClick={handleClick}
-        >
-          Learn KendoReact
-        </Button>
-      </header>
+      <IntlProvider locale={contextState.localeId}></IntlProvider>
     </div>
   );
 }
