33e640030fcaf59028b18a2aef0ed4863e0088e6
unknown <cksdyd8350@gmail.com>
2023-06-12 17:10:41 +0900

    modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import { Button } from '@progress/kendo-react-buttons';
-import kendoka from './kendoka.svg';
-import './App.scss';
-import { IntlProvider, load, LocalizationProvider, loadMessages } from '@progress/kendo-react-intl';
+import React from "react";
+import { Button } from "@progress/kendo-react-buttons";
+import kendoka from "./kendoka.svg";
+import "./App.scss";
+import {
+  IntlProvider,
+  load,
+  LocalizationProvider,
+  loadMessages,
+} from "@progress/kendo-react-intl";
+import TeamEfficiencyChart from "./components/Chart";
+import Gridmember from "./components/Gridmembers";
 
 function App() {
   const [contextState, setContextState] = React.useState({
-    localeId: 'en-US',
-    firstName: 'Peter',
-    lastName: 'Douglas',
-    middleName: '',
-    email: 'peter.douglas@progress.com',
-    phoneNumber: '(+1) 8373-837-93-02',
+    localeId: "en-US",
+    firstName: "Peter",
+    lastName: "Douglas",
+    middleName: "",
+    email: "peter.douglas@progress.com",
+    phoneNumber: "(+1) 8373-837-93-02",
     avatar: null,
     isInPublicDirectory: true,
-    biography: '',
-    teamId: 1
-});
+    biography: "",
+    teamId: 1,
+  });
 
   return (
     <div className="App">
-      <IntlProvider locale={contextState.localeId}></IntlProvider>
+      <TeamEfficiencyChart></TeamEfficiencyChart>
+      <Gridmember></Gridmember>
     </div>
   );
 }
---
+++ src/components/Gridmembers.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Gridmember = () => {
+  return (
+    <div>
+      <div className="card-container grid">
+        <h3 className="card-title">team members</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Gridmember;
