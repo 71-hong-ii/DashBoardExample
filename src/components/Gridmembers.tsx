a6a3b01d53be9d0d287a36520490a4bee087e927
unknown <cksdyd8350@gmail.com>
2023-06-14 17:02:54 +0900

    enabled filtering and grouping in gridmembers.tsx

--- src/components/Gridmembers.tsx
+++ src/components/Gridmembers.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, SetStateAction, JSXElementConstructor } from "react";
 import {
   Grid,
   GridColumn,
@@ -8,8 +8,10 @@ import {
   GridFilterChangeEvent,
   GridExpandChangeEvent,
   GridGroupChangeEvent,
+  GridDataStateChangeEvent,
+  GridCellProps,
 } from "@progress/kendo-react-grid";
-//import Grid from "./Grid";
+
 import {
   IntlProvider,
   load,
@@ -23,65 +25,133 @@ import {
   GroupDescriptor,
   groupBy,
   GroupResult,
+  State,
+  DataResult,
+  process,
+  AggregateDescriptor,
 } from "@progress/kendo-data-query";
-import { setGroupIds } from "@progress/kendo-react-data-tools";
+import {
+  setExpandedState,
+  setGroupIds,
+} from "@progress/kendo-react-data-tools";
 import { employees } from "./../resources/employees";
 import { teams } from "./../resources/teams";
 import { orders } from "./../resources/orders";
 import { employee } from "./../interfaces/employee";
 
-const initialFilter: CompositeFilterDescriptor = {
-  logic: "and",
-  filters: [{ field: "fullName", operator: "contains", value: "" }],
-};
-const initialGroup: GroupDescriptor[] = [{ field: "rating" }];
-const processWithGroups = (data: employee[], group: GroupDescriptor[]) => {
-  const newDataState = groupBy(data, group);
 
-  setGroupIds({ data: newDataState, group: group });
+const initialDataState: State = {
+  take: 10,
+  skip: 0,
+  group: [{ field: "rating" }],
+};
+
+const aggregates: AggregateDescriptor[] = [
+  { field: "rating", aggregate: "average" },
+  { field: "budget", aggregate: "average" },
+];
+
+const processWithGroups = (data: employee[], dataState: State) => {
+  const groups = dataState.group;
+  if (groups) {
+    groups.map((group) => (group.aggregates = aggregates));
+  }
+  dataState.group = groups;
+  const newDataState = process(data, dataState);
+
+  setGroupIds({ data: newDataState.data, group: dataState.group });
 
   return newDataState;
 };
 
-const Gridmember = () => {
-  const [filter, setFilter] = useState(initialFilter);
-  const [group, setGroup] = useState(initialGroup);
-  const [resultState, setResultState] = useState<GroupResult[]>(
-    processWithGroups(employees, initialGroup)    
-  );
-  const onGroupChange = React.useCallback((event: GridGroupChangeEvent) => {
-    const newDataState = processWithGroups(employees, event.group);
 
-    setGroup(event.group);
-    setResultState(newDataState);
-  }, []);
-  
+
+const Gridmember = () => {
+  const [dataState, setDataState] = React.useState<State>(initialDataState);
+  const [result, setResult] = React.useState<DataResult>(
+    processWithGroups(employees, initialDataState)
+  );
+  const [collapsedState, setCollapsedState] = React.useState<string[]>([]);
+
+
+
+  const dataStateChange = (event: GridDataStateChangeEvent) => {
+    const newDataState = processWithGroups(employees, event.dataState);
+    setResult(newDataState);
+    setDataState(event.dataState);
+  };
+
+  const expandChange = (event: GridExpandChangeEvent) => {
+    const item = event.dataItem;
+
+    if (item.groupId) {
+      const newCollapsedIds = !event.value
+        ? [...collapsedState, item.groupId]
+        : collapsedState.filter((groupId) => groupId !== item.groupId);
+      setCollapsedState(newCollapsedIds);
+    }
+  };
+
+  const cellRender = (
+    tdElement: React.ReactElement<HTMLTableCellElement, string | JSXElementConstructor<any>> | null,
+    cellProps: GridCellProps
+  ): JSX.Element => {
+    if (cellProps.rowType === "groupFooter") {
+      if (cellProps.field === "UnitPrice") {
+        return (
+          <td aria-colindex={cellProps.columnIndex} role="gridcell">
+            Average: {cellProps.dataItem.aggregates.UnitPrice.average}
+          </td>
+        );
+      } else if (cellProps.field === "UnitsInStock") {
+        return (
+          <td aria-colindex={cellProps.columnIndex} role="gridcell">
+            Sum: {cellProps.dataItem.aggregates.UnitsInStock.sum}
+          </td>
+        );
+      }
+    }
+    return tdElement as JSX.Element;
+  };
+  const newData = setExpandedState({
+    data: result.data,
+    collapsedIds: collapsedState,
+  });
+
+
+
   return (
     <div>
       <div className="card-container grid">
-        <h3 className="card-title">team members</h3>
+        <h3 className="card-title">Team members</h3>
       </div>
       <span></span>
       <Grid
-        style={{
-          height: "400px",
-        }}
-        data={filterBy(employees, filter)}
+        style={{ height: "520px" }}
+        resizable={true}
+        reorderable={true}
         filterable={true}
-        filter={filter}
-        onFilterChange={(e: GridFilterChangeEvent) => setFilter(e.filter)}
+        sortable={true}
+        pageable={{ pageSizes: true }}
+        groupable={{ footer: "visible" }}
+        data={newData}
+        onDataStateChange={dataStateChange}
+        {...dataState}
+        onExpandChange={expandChange}
+        expandField="expanded"
+        cellRender={cellRender}
       >
-        <GridColumn field="employees" title="Employee">
+        <GridColumn field="employees" title="Employee" groupable={false}>
           <GridColumn field="fullName" title="fullName"></GridColumn>
           <GridColumn field="jobTitle" title="jobTitle"></GridColumn>
           <GridColumn field="country" title="country"></GridColumn>
         </GridColumn>
-        <GridColumn title="Performance">
+        <GridColumn title="Performance" groupable={false}>
           <GridColumn field="rating" filter="numeric"></GridColumn>
           <GridColumn field="target"></GridColumn>
           <GridColumn field="budget" filter="numeric"></GridColumn>
         </GridColumn>
-        <GridColumn title="Contacts">
+        <GridColumn title="Contacts" groupable={false}>
           <GridColumn field="phone"></GridColumn>
           <GridColumn field="address"></GridColumn>
         </GridColumn>
