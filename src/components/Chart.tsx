45ff1d85b2dd6345ab9c4a3e17b1c1e8133af5a4
unknown <cksdyd8350@gmail.com>
2023-06-14 14:24:59 +0900

    modified path of Chart.tsx

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { Chart, ChartSeries, ChartSeriesItem, ChartCategoryAxis, ChartCategoryAxisItem, ChartTitle, ChartLegend } from '@progress/kendo-react-charts';
 
-import teams from '/Users/gimgihong/Documents/2023 - 1/2023산학협력프로젝트/kendoReactApplicationWithGit/DashBoardExample/src/resources/teams.js';
+import {teams} from './../resources/teams.js';
 
 // 데이터를 정의합니다. 이는 실제 데이터에 따라 변경되어야 합니다.
 const categories = ["January 2020", "February 2020", "March 2020", "April 2020"];
