26f67d93f590df9bcc1c1135ff411f4f10751aa4
unknown <cksdyd8350@gmail.com>
2023-06-16 14:14:58 +0900

    Applied tileLayout to employees.tsx

--- src/pages/employees.tsx
+++ src/pages/employees.tsx
@@ -1,9 +1,54 @@
+import { Grid } from "@progress/kendo-react-grid";
+import { TileLayout } from "@progress/kendo-react-layout";
 import Gridmember from "../components/Gridmembers";
 
+const tiles = [
+  {
+    defaultPosition: {
+      col: 1,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "First is",
+    body: "one",
+  },
+  {
+    defaultPosition: {
+      col: 2,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Second is",
+    body: "two",
+  },
+  {
+    defaultPosition: {
+      col: 3,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Third is",
+    body: "three",
+  },
+];
+
+const dataTiles = [
+  {
+    defaultPosition: {
+      col: 1,
+      colSpan: 1,
+      rowSpan: 1,
+    },
+    header: "Team members tileLayout here",
+    body: <Gridmember />,
+  },
+];
+
 export const Employees = () => {
-    return (
-      <div>
-        <Gridmember />
-      </div>
-    );
-}
+  return (
+    <div>
+      <TileLayout columns={3} items={tiles} rowHeight={300}></TileLayout>
+      <TileLayout columns={1} items={dataTiles}></TileLayout>
+    </div>
+  );
+};
