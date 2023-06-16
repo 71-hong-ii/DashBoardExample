79668205a9b0c15e4c97de4da9eb9c2dac705f4e
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-16 13:53:36 +0900

    asdf

--- src/components/homeLayout.tsx
+++ src/components/homeLayout.tsx
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "@progress/kendo-react-buttons";
-import { useLocation, useNavigate, Outlet, Link } from "react-router-dom";
-import { Drawer, DrawerContent } from "@progress/kendo-react-layout";
+import React, { useState, useEffect } from 'react';
+import { Button } from '@progress/kendo-react-buttons';
+import { useLocation, useNavigate, Outlet, Link } from 'react-router-dom';
+import { Drawer, DrawerContent } from '@progress/kendo-react-layout';
+import { Icon } from '@progress/kendo-react-common';
+import './HomeLayout.scss';
 
 export const items = [
   {
-    text: "Employees",
+    text: 'Employees',
     selected: true,
-    route: "/employees",
-    icon: "k-i-grid",
+    route: '/employees',
+    icon: 'k-i-grid',
   },
   {
-    text: "Orders",
+    text: 'Orders',
     selected: false,
-    route: "/orders",
-    icon: "chart-line-markers",
+    route: '/orders',
+    icon: 'chart-line-markers',
+  },
+  {
+    text: 'Home',
+    selected: false,
+    route: '/',
+    icon: 'home',
   },
 ];
 
@@ -22,7 +30,7 @@ export const HomeLayout = (props: any) => {
   const navigate = useNavigate();
   const location = useLocation();
   const [expanded, setExpanded] = useState(true);
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState('');
 
   const handleClick = () => {
     setExpanded(!expanded);
@@ -39,21 +47,31 @@ export const HomeLayout = (props: any) => {
     }
   }, [location.pathname]);
 
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <div className="custom-toolbar">
-        <Button icon="menu" onClick={handleClick} />
+        <Button icon="menu" onClick={handleClick} className="menu-button" />
 
-        <span className="overview">
-          {selected === "Employees" ? "Overview" : selected}
+        <span className={selected === 'Employees' || selected === 'Orders' ? 'selected-text' : ''}>
+          {selected === 'Employees' ? 'Employees' : selected === 'Orders' ? 'Orders' : 'Home'}
         </span>
+
+        {selected !== 'Home' && (
+          <Button onClick={handleHomeClick} className="home-button">
+            <Icon name="home" />
+          </Button>
+        )}
       </div>
 
       <div>
         <Drawer
           expanded={expanded}
-          position={"start"}
-          mode={"push"}
+          position="start"
+          mode="push"
           width={240}
           items={items.map((item) => ({
             ...item,
@@ -64,7 +82,7 @@ export const HomeLayout = (props: any) => {
         >
           <DrawerContent>
             {props.children}
-            <Outlet />{" "}
+            <Outlet />
           </DrawerContent>
         </Drawer>
       </div>
