9539c264c59714bd6e684e3960f49c293669e112
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-16 14:17:50 +0900

    :wq

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { Button } from "@progress/kendo-react-buttons";
-import kendoka from "./kendoka.svg";
-import "./App.scss";
-import {
-  IntlProvider,
-  load,
-  LocalizationProvider,
-  loadMessages,
-} from "@progress/kendo-react-intl";
-import { Employees } from "./pages/employees"
-import { Home } from "./pages/home";
-import { Orders } from "./pages/orders";
-import { HomeLayout } from "./components/homeLayout";
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Button } from '@progress/kendo-react-buttons';
+import kendoka from './kendoka.svg';
+import './App.scss';
+import {Employees} from './pages/employees';
+import Orders from './pages/orders';
+import {Home} from './pages/home';
+import {HomeLayout} from './components/homeLayout';
 
 function App() {
   return (
     <div className="App">
-      <HomeLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/employees" element={<Employees />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      </HomeLayout>
+      <HashRouter>
+        <HomeLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </HomeLayout>
+      </HashRouter>
     </div>
   );
 }
--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,11 +1,39 @@
-import PieChart from "./../components/PieChart"
-import BarChart from "./../components/BarChart"
+import React, { useState } from 'react';
+import { Responsive, WidthProvider } from 'react-grid-layout';
+import { Chart, ChartSeries, ChartSeriesItem, ChartCategoryAxis, ChartCategoryAxisItem, ChartTitle, ChartLegend, ChartTooltip } from '@progress/kendo-react-charts';
+import { TooltipContext, SharedTooltipContext } from '@progress/kendo-react-charts';
+import PieChart from '../components/PieChart';
+import BarChart from '../components/BarChart';
 
-export const Orders = () => {
-    return (
-      <div>
-          <PieChart />
-          <BarChart />
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+const Orders = () => {
+  const [layout, setLayout] = useState([
+    { i: 'pie', x: 0, y: 0, w: 6, h: 6 },
+    { i: 'bar', x: 6, y: 0, w: 6, h: 6 },
+  ]);
+
+  const onLayoutChange = (newLayout: any[]) => {
+    setLayout(newLayout);
+  };
+
+  return (
+    <ResponsiveGridLayout
+      className="layout"
+      layouts={{ lg: layout }}
+      breakpoints={{ lg: 1200 }}
+      cols={{ lg: 12 }}
+      rowHeight={100}
+      onLayoutChange={onLayoutChange}
+    >
+      <div key="pie">
+        <PieChart />
       </div>
-    );
-   }
+      <div key="bar">
+        <BarChart />
+      </div>
+    </ResponsiveGridLayout>
+  );
+};
+
+export default Orders;
