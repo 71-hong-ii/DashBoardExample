f93de2e6c9b1e7fdef9cc3349efebf61adf8d5a3
김기홍 <gimgihong@gimgihong-ui-MacBookPro.local>
2023-06-16 13:31:32 +0900

    modified BarCahrt.tsx

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,57 +1,85 @@
-// 필요한 모듈을 import 합니다.
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart, ChartSeries, ChartSeriesItem, ChartCategoryAxis, ChartCategoryAxisItem, ChartTitle, ChartLegend, ChartTooltip } from '@progress/kendo-react-charts';
-// ChartSeriesItemTooltip 컴포넌트를 import 합니다.
-import { ChartSeriesItemTooltip } from '@progress/kendo-react-charts';
+import { TooltipContext, SharedTooltipContext } from '@progress/kendo-react-charts';
 
-import { teamOrders } from '../resources/teamOrders.js';
-import { categories } from '../resources/categories.js';
+interface OrderData {
+  teamID: string;
+}
 
-const data = Object.entries(teamOrders).map(([teamID, count], index) => ({
-  category: `Team ${teamID}`,
-  value: count,
-  color: index === 0 ? 'rgb(252, 74, 70)' :
-         index === 1 ? 'rgb(255, 221, 80)' :
-         index === 2 ? 'rgb(65, 202, 108)' :
-                              'rgb(58, 65, 249)',
-}));
+const BarChart: React.FC = () => {
+  const [teamOrders, setTeamOrders] = useState<{ [key: string]: number }>({});
 
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch('http://13.59.95.158:8000/data/orders');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: OrderData[] = await response.json();
 
+        const orders: { [teamID: string]: number } = {};
+        data.forEach(order => {
+          const teamID = order.teamID;
+          if (teamID) {
+            if (orders[teamID]) {
+              orders[teamID] += 1;
+            } else {
+              orders[teamID] = 1;
+            }
+          }
+        });
+        setTeamOrders(orders);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    };
 
-// 툴팁을 렌더링하는 컴포넌트를 정의합니다.
-const OrderTooltip: React.FC<any> = ({ point }) => {
-  if (!point) {
-    return null; // point가 undefined인 경우 null을 반환하여 아무것도 렌더링하지 않음
-  }
+    fetchOrders();
+  }, []);
 
-  const totalOrders = Object.values(teamOrders).reduce((a, b) => a + b, 0);
-  const orderPercentage = ((point.value / totalOrders) * 100).toFixed(2);
+  const categories = Object.keys(teamOrders).map((teamID) => `Team ${teamID}`);
+  const data = Object.entries(teamOrders).map(([teamID, count]) => ({
+    category: `Team ${teamID}`,
+    value: count,
+  }));
+
+  const OrderTooltip = (props: TooltipContext | SharedTooltipContext): React.ReactNode => {
+    const point = 'point' in props ? props.point : undefined;
+    if (!point) {
+      return null;
+    }
+  
+    const totalOrders = Object.values(teamOrders).reduce((a, b) => a + b, 0);
+    const orderPercentage = ((point.value / totalOrders) * 100).toFixed(2);
+  
+    return (
+      <div>
+        <h3>{String(point.category)}</h3>
+        <p>Order Percentage: {orderPercentage}%</p>
+        <p>Total Orders: {String(point.value)}</p>
+      </div>
+    );
+  };
 
   return (
-    <div>
-      <h3>{point.category}</h3>
-      <p>Order Percentage: {orderPercentage}%</p>
-      <p>Total Orders: {point.value}</p>
-    </div>
+    <Chart>
+      <ChartTitle text="Team Order Distribution" />
+      <ChartLegend position="top" orientation="horizontal" />
+      <ChartTooltip render={OrderTooltip} />
+      <ChartCategoryAxis>
+        <ChartCategoryAxisItem categories={categories} />
+      </ChartCategoryAxis>
+      <ChartSeries>
+        <ChartSeriesItem
+          type="column"
+          data={data}
+          field="value"
+          categoryField="category"
+        />
+      </ChartSeries>
+    </Chart>
   );
 };
-
-
-// 차트 컴포넌트를 정의합니다.
-const BarChart: React.FC = () => (
-  <Chart>
-    <ChartTitle text="Team Order Distribution" />
-    <ChartLegend position="top" orientation="horizontal" />
-    <ChartTooltip render={OrderTooltip} />
-    <ChartCategoryAxis>
-      <ChartCategoryAxisItem categories={categories} />
-    </ChartCategoryAxis>
-    <ChartSeries>
-      <ChartSeriesItem type="bar" data={data} field="value" categoryField="category">
-        <ChartSeriesItemTooltip render={OrderTooltip} />
-      </ChartSeriesItem>
-    </ChartSeries>
-  </Chart>
-);
 
 export default BarChart;
