a4e64f1f0a83d3275e7ef25959924762a6c497a4
unknown <cksdyd8350@gmail.com>
2023-06-16 12:45:51 +0900

    Connected data to server

--- src/components/Gridmembers.tsx
+++ src/components/Gridmembers.tsx
@@ -1,4 +1,9 @@
-import React, { useState, SetStateAction, JSXElementConstructor } from "react";
+import React, {
+  useState,
+  useEffect,
+  SetStateAction,
+  JSXElementConstructor,
+} from "react";
 import {
   Grid,
   GridColumn,
@@ -22,7 +27,7 @@ import {
   filterBy,
   CompositeFilterDescriptor,
   GroupDescriptor,
-  groupBy,  
+  groupBy,
   GroupResult,
   State,
   DataResult,
@@ -33,11 +38,13 @@ import {
   setExpandedState,
   setGroupIds,
 } from "@progress/kendo-react-data-tools";
-import  employees  from "./../resources/employees_json.json";
+import employees from "./../resources/employees_json.json";
 import { teams } from "./../resources/teams";
 import { orders } from "./../resources/orders";
 import { employee } from "./../interfaces/employee";
 import { HomeLayout } from "./homeLayout";
+import axios from "axios";
+import { promiseHooks } from "v8";
 
 const initialDataState: State = {
   take: 10,
@@ -63,34 +70,49 @@ const processWithGroups = (data: employee[], dataState: State) => {
   return newDataState;
 };
 
+let arr: employee[] = [];
+function getDataFromServer() {
 
-const getDataFromServer  = () =>{
-  const Http = new XMLHttpRequest();
-  const url = "https://4quunovf83.execute-api.us-east-2.amazonaws.com/default/MyFastAPI1";
+  axios
+    .get("http://13.59.95.158:8000/data/employees", { withCredentials: true })
+    .then((response) => {
+      arr = response.data;
+      console.log(arr[0]);
+    })
+    .catch((error) => {});
 
-  Http.open("GET", url);
-  Http.send();
-
-  console.log("hihi\n");
-  console.log(Http.responseText);
 }
 
 const Gridmember = () => {
-  const [dataState, setDataState] = React.useState<State>(initialDataState);
+  const [dataState, setDataState] = useState<State>(initialDataState);
+  //replace employees to..
   const [result, setResult] = React.useState<DataResult>(
-    processWithGroups(employees, initialDataState)
+    processWithGroups(arr, initialDataState)
   );
-  const [collapsedState, setCollapsedState] = React.useState<string[]>([]);
+  const [collapsedState, setCollapsedState] = useState<string[]>([]);
+
+  
+  useEffect(() => {
+    console.log(arr[0])
+    axios
+      .get("http://13.59.95.158:8000/data/employees", { withCredentials: true })
+      .then((response) => {
+        arr = response.data;
+        console.log(arr[0]);
+      })
+      .catch((error) => {});
+      
+  }, []);
+
 
   const newData = setExpandedState({
-
-
     data: result.data,
     collapsedIds: collapsedState,
   });
 
+  //and here
   const dataStateChange = (event: GridDataStateChangeEvent) => {
-    const newDataState = processWithGroups(employees, event.dataState);
+    const newDataState = processWithGroups(arr, event.dataState);
     setResult(newDataState);
     setDataState(event.dataState);
   };
@@ -107,7 +129,10 @@ const Gridmember = () => {
   };
 
   const cellRender = (
-    tdElement: React.ReactElement<HTMLTableCellElement, string | JSXElementConstructor<any>> | null,
+    tdElement: React.ReactElement<
+      HTMLTableCellElement,
+      string | JSXElementConstructor<any>
+    > | null,
     cellProps: GridCellProps
   ): JSX.Element => {
     if (cellProps.rowType === "groupFooter") {
@@ -128,44 +153,43 @@ const Gridmember = () => {
     return tdElement as JSX.Element;
   };
 
-
   return (
     <div>
-        <div className="card-container grid">
-          <h3 className="card-title">Team members</h3>
-        </div>
-        <span></span>
-        <Grid
-          style={{ height: "520px" }}
-          resizable={true}
-          reorderable={true}
-          filterable={true}
-          sortable={true}
-          pageable={{ pageSizes: true }}
-          total={result.total}
-          groupable={{ footer: "visible" }}
-          data={newData}
-          onDataStateChange={dataStateChange}
-          {...dataState}
-          onExpandChange={expandChange}
-          expandField="expanded"
-          cellRender={cellRender}
-        >
-          <GridColumn field="employees" title="Employee" groupable={false}>
-            <GridColumn field="fullName" title="fullName"></GridColumn>
-            <GridColumn field="jobTitle" title="jobTitle"></GridColumn>
-            <GridColumn field="country" title="country"></GridColumn>
-          </GridColumn>
-          <GridColumn title="Performance" groupable={false}>
-            <GridColumn field="rating" filter="numeric"></GridColumn>
-            <GridColumn field="target"></GridColumn>
-            <GridColumn field="budget" filter="numeric"></GridColumn>
-          </GridColumn>
-          <GridColumn title="Contacts" groupable={false}>
-            <GridColumn field="phone"></GridColumn>
-            <GridColumn field="address"></GridColumn>
-          </GridColumn>
-        </Grid>
+      <div className="card-container grid">
+        <h3 className="card-title">Team members</h3>
+      </div>
+      <span></span>
+      <Grid
+        style={{ height: "520px" }}
+        resizable={true}
+        reorderable={true}
+        filterable={true}
+        sortable={true}
+        pageable={{ pageSizes: true }}
+        total={result.total}
+        groupable={{ footer: "visible" }}
+        data={newData}
+        onDataStateChange={dataStateChange}
+        {...dataState}
+        onExpandChange={expandChange}
+        expandField="expanded"
+        cellRender={cellRender}
+      >
+        <GridColumn field="employees" title="Employee" groupable={false}>
+          <GridColumn field="fullName" title="fullName"></GridColumn>
+          <GridColumn field="jobTitle" title="jobTitle"></GridColumn>
+          <GridColumn field="country" title="country"></GridColumn>
+        </GridColumn>
+        <GridColumn title="Performance" groupable={false}>
+          <GridColumn field="rating" filter="numeric"></GridColumn>
+          <GridColumn field="target"></GridColumn>
+          <GridColumn field="budget" filter="numeric"></GridColumn>
+        </GridColumn>
+        <GridColumn title="Contacts" groupable={false}>
+          <GridColumn field="phone"></GridColumn>
+          <GridColumn field="address"></GridColumn>
+        </GridColumn>
+      </Grid>
     </div>
   );
 };
